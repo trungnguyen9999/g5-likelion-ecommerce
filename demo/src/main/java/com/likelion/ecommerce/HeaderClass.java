61044a4fc11b5270c981b510806115039182933b
trungnguyen9999 <trungnguyen.prm@gmail.com>
2024-06-30 16:09:03 +0700

    Accept all origin

--- demo/src/main/java/com/likelion/ecommerce/HeaderClass.java
+++ demo/src/main/java/com/likelion/ecommerce/HeaderClass.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpHeaders;
 public class HeaderClass {
 	public static HttpHeaders getHeader()	{
 		HttpHeaders headers = new HttpHeaders();
-        headers.add("Access-Control-Allow-Origin", "https://g5-likelion-ecommerce.onrender.com");
+        headers.add("Access-Control-Allow-Origin", "*");
         return headers;
 	}
 }
